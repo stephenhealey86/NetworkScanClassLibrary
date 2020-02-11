558e9a0a0be303eddfb9809c413601a83da6417b
Stephen Healey <s_healey@hotmail.co.uk>
2020-02-11 22:08:12 +0000

    Add project files.

---
+++ NetworkScanClassLibrary/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkScanClassLibrary
+{
+    public class Class1
+    {
+    }
+}
